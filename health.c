16db8cebe8b8f4d44bc45b26c77af48bc50bb7ef
ehaisa <isalaura1304@gmail.com>
2024-03-08 13:06:23 -0300

    criando tela principal

---
+++ health.c
@@ -0,0 +1,38 @@
+// ESTE PROGRAMA ESTÁ SENDO FEITO INSPIRADO NO MATERIAL DE APOIO DISPONIBILIZADO PELO PROFESSOR FLAVIUS
+// GORGÔNIO, disponível em @FlaviusGorgonio https://github.com/FlaviusGorgonio/LinguaSolta.git
+
+#include <stdio.h>
+
+// Assinatura das funções
+void telaMain(void);
+
+// Programa Principal
+int main(void) {
+    telaMain();
+}
+
+// Funções
+void telaMain(void) {
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("||                                                                           ||\n");
+    printf("||               ===========================================                 ||\n");
+    printf("||               =                                         =                 ||\n");
+    printf("||               =             Clínica em Casa             =                 ||\n");
+    printf("||               =                                         =                 ||\n");
+    printf("||               ===========================================                 ||\n");
+    printf("||              Desenvolvido por @ehaisa -- desde Março, 2024                ||\n");
+    printf("||                                                                           ||\n");
+    printf("===============================================================================\n");
+    printf("||                                                                           ||\n");
+    printf("||                     = = = = Tela Principal = = = =                        ||\n");
+    printf("||                                                                           ||\n");
+    printf("||             1. Fichas de Pacientes                                        ||\n");
+    printf("||             2. Calendário                                                 ||\n");
+    printf("||             3. Serviços                                                   ||\n");
+    printf("||             4. Relatórios                                                 ||\n");
+    printf("||             0. Sair                                                       ||\n");
+    printf("||                                                                           ||\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
