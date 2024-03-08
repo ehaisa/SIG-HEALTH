311dd417ec21e7eb795548b8f6e6bddd85fc4af6
ehaisa <isalaura1304@gmail.com>
2024-03-08 13:46:40 -0300

    criando tela de informações

--- health.c
+++ health.c
@@ -5,10 +5,12 @@
 
 // Assinatura das funções
 void telaMain(void);
+void telaSobre(void);
 
 // Programa Principal
 int main(void) {
     telaMain();
+    telaSobre();
 }
 
 // Funções
@@ -34,5 +36,32 @@ void telaMain(void) {
     printf("||             0. Sair                                                       ||\n");
     printf("||                                                                           ||\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void telaSobre(void) {
+    printf("\n");
+    printf("|| ========================================================================= ||\n");
+    printf("||                                                                           ||\n");
+    printf("||                Universidade Federal do Rio Grande do Norte                ||\n");
+    printf("||                    Centro de Ensino Superior do Seridó                    ||\n");
+    printf("||                 Departamento de Computação e Tecnologia                   ||\n");
+    printf("||                     Disciplina DCT1106 -- Programação                     ||\n");
+    printf("||           Sistema de Gestão para Atendimento de Saúde a Domicílio         ||\n");
+    printf("||              Desenvolvido por @ehaisa -- desde Março, 2024                ||\n");
+    printf("||                                                                           ||\n");
+    printf("|| ========================================================================= ||\n");
+    printf("||                                                                           ||\n");
+    printf("||                  = = = = Propósito do Projeto = = = =                     ||\n");
+    printf("||                                                                           ||\n");
+    printf("||   Esse programa não foi feito para uso comercial, sendo desenvolvido      ||\n");
+    printf("||   por uma aluna do curso Bacharelado em Sistemas da Informação, da        ||\n");
+    printf("||   da UFRN, CERES - CAICÓ, e tendo uma natureza estritamente acadêmica     ||\n");
+    printf("||   endo feito para a aplicação prática dos conceitos repassados e          ||\n");
+    printf("||   discutidos em sala de aula. O programa também tem propósito avaliativo, ||\n");
+    printf("||   sendo uma ferramenta para avaliar o desempenho da desenvolvedora e      ||\n");
+    printf("||   a sua compreensão do assunto.                                           ||\n");
+    printf("||                                                                           ||\n");
+    printf("|| ========================================================================= ||\n");
     printf("\n");
 }
