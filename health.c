34865187397229a689bfb44663ac34b128746a8d
ehaisa <isalaura1304@gmail.com>
2024-03-15 16:16:54 -0300

    add tela de editar paciente

--- health.c
+++ health.c
@@ -13,6 +13,7 @@ void telaCalendario(void);
 void telaServicos(void);
 void criarPaciente(void);
 void lerPaciente(void);
+void editarPaciente(void);
 
 // Programa Principal
 int main(void) {
@@ -20,6 +21,7 @@ int main(void) {
     telaPacientes();
     criarPaciente();
     lerPaciente();
+    editarPaciente();
     telaCalendario();
     telaServicos();
     telaSobre();
@@ -109,6 +111,34 @@ void lerPaciente(void) {
     printf("||                                                                           ||\n");
     printf("|| ========================================================================= ||\n");
     printf("\n");
+    printf("\t      Aperte a tecla <ENTER> para continuar a operação. !! \n");
+    getchar();
+}
+
+void editarPaciente(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("|| ========================================================================= ||\n");
+    printf("||                                                                           ||\n");
+    printf("||               ===========================================                 ||\n");
+    printf("||               =                                         =                 ||\n");
+    printf("||               =             Clínica em Casa             =                 ||\n");
+    printf("||               =                                         =                 ||\n");
+    printf("||               ===========================================                 ||\n");
+    printf("||              Desenvolvido por @ehaisa -- desde Março, 2024                ||\n");
+    printf("||                                                                           ||\n");
+    printf("|| ========================================================================= ||\n");
+    printf("||                                                                           ||\n");
+    printf("||                      = = = = Editar Ficha = = = =                         ||\n");
+    printf("||                                                                           ||\n");
+    printf("||             1. Data de Nascimento                                         ||\n");
+    printf("||             2. Número de Contato                                          ||\n");
+    printf("||             3. Condições Médicas                                          ||\n");
+    printf("||             0. Sair                                                       ||\n");
+    printf("||                                                                           ||\n");
+    printf("|| ========================================================================= ||\n");
+    printf("\n");
+    printf("\t      Informe a opção que deseja modificar ou adicionar. \n");
     printf("\t      Aperte a tecla <ENTER> para continuar a operação. !! \n");
     getchar();
 }
