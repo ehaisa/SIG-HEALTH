71dcbb97582f24f63dc44e21370f341405c877a0
ehaisa <isalaura1304@gmail.com>
2024-04-03 17:53:00 -0300

    Add comentários de créditos

--- health.c
+++ health.c
@@ -1,3 +1,9 @@
+// ESTE PROGRAMA ESTÁ SENDO FEITO INSPIRADO NO MATERIAL DE APOIO DISPONIBILIZADO PELO PROFESSOR FLAVIUS
+// GORGÔNIO, disponível em @FlaviusGorgonio https://github.com/FlaviusGorgonio/LinguaSolta.git
+// ASSIM COMO ESTÁ SENDO PRODUZIDO COM INSPIRAÇÃO E O AUXÍLIO DE HILDEMBERG ELING DE ARAUJO LUCENA @Hildemberg986 
+// disponível em https://github.com/Hildemberg986/projetoHotel.git
+// E UTILIZAÇÃO DO CONTEÚDO E MATERIAIS VISTOS E DADOS EM SALA DE AULA
+
 #include "./telas/telasmenu.h"
 #include "./logica/logic_telamain.h"
 
